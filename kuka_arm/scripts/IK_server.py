3c22d3082ed36cb32c7c1911e9495e966c87dd31
Jonathan Luu <jrluu1@gmail.com>
2019-02-03 01:43:17 -0800

    Fixed algorithm to put in a proper place, will move things to make it more efficient

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -7,7 +7,7 @@
 #
 # All Rights Reserved.
 
-# Author: Jon
+# Author: Harsh Pandya
 
 # import modules
 import rospy
@@ -19,15 +19,6 @@ from mpmath import *
 from sympy import *
 
 
-def TF_Matrix(alpha, a, d, q):
-    TF = Matrix([[cos(q)             ,                -sin(q),           0,               a ],
-        [sin(q) * cos(alpha),    cos(q) * cos(alpha), -sin(alpha), -sin(alpha) * d ],
-        [sin(q) * sin(alpha),    cos(q) * sin(alpha),  cos(alpha),  cos(alpha) * d ],
-        [0                  ,                      0,           0,                1]])
-    return TF
-
-
-
 def handle_calculate_IK(req):
     rospy.loginfo("Received %s eef-poses from the plan" % len(req.poses))
     if len(req.poses) < 1:
@@ -36,7 +27,7 @@ def handle_calculate_IK(req):
     else:
 
         ### Your FK code here
-        #DH Paramters
+        # Create symbols
         d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8') #link offset
         a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7') # link length
         alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6, = symbols('alpha0:7') #twist angle
@@ -44,7 +35,7 @@ def handle_calculate_IK(req):
         #Joint angle symbols
         q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
 
-        #Modified DH Params
+	# Create Modified DH parameters
         DH_Table = {  alpha0:       0, a0:      0, d1:  0.75, q1:        q1 , 
                       alpha1: -pi/2.0, a1:   0.35, d2:   0.0, q2:  q2 - pi/2,
                       alpha2:       0, a2:   1.25, d3:   0.0, q3:        q3 ,
@@ -53,6 +44,15 @@ def handle_calculate_IK(req):
                       alpha5: -pi/2.0, a5:      0, d6:     0, q6:        q6 ,
                       alpha6:       0, a6:      0, d7: 0.303, q7:         0 }
 
+	# Define Modified DH Transformation matrix
+        def TF_Matrix(alpha, a, d, q):
+            TF = Matrix([[cos(q)             ,                -sin(q),           0,               a ],
+                [sin(q) * cos(alpha),    cos(q) * cos(alpha), -sin(alpha), -sin(alpha) * d ],
+                [sin(q) * sin(alpha),    cos(q) * sin(alpha),  cos(alpha),  cos(alpha) * d ],
+                [0                  ,                      0,           0,                1]])
+            return TF
+
+	# Create individual transformation matrices
         T0_1 = TF_Matrix(alpha0, a0, d1, q1).subs(DH_Table)
         T1_2 = TF_Matrix(alpha1, a1, d2, q2).subs(DH_Table)
         T2_3 = TF_Matrix(alpha2, a2, d3, q3).subs(DH_Table)
@@ -61,29 +61,11 @@ def handle_calculate_IK(req):
         T5_6 = TF_Matrix(alpha5, a5, d6, q6).subs(DH_Table)
         T6_EE = TF_Matrix(alpha6, a6, d7, q7).subs(DH_Table)
 
-        T0_EE = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_EE
-
-
-        r, p, y = symbols('r p y')
-
-
-        ROT_X = Matrix([[1,      0,       0],
-                        [0, cos(r), -sin(r)],
-                        [0, sin(r),  cos(r)]])  #ROLL
-
-        ROT_Y = Matrix([[cos(p),  0, sin(p)],
-                        [     0,  1,      0],
-                        [-sin(p), 0, cos(p)]]) #PITCH
-
-        ROT_Z = Matrix([[cos(y), -sin(y), 0],
-                        [sin(y),  cos(y), 0],
-                        [0,            0, 1]]) #YAW
-
-        ROT_EE = ROT_Z * ROT_Y * ROT_X
-
-        ROT_Error = ROT_Z.subs(y, radians(180)) * ROT_Y.subs(p, radians(-90))
-
-        ROT_EE = ROT_EE * ROT_Error
+        T0_EE = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_EE   
+	# Extract rotation matrices from the transformation matrices
+	#
+	#
+        ###
 
         # Initialize service response
         joint_trajectory_list = []
@@ -101,6 +83,29 @@ def handle_calculate_IK(req):
             (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
                 [req.poses[x].orientation.x, req.poses[x].orientation.y,
                     req.poses[x].orientation.z, req.poses[x].orientation.w])
+  
+
+	    # Compensate for rotation discrepancy between DH parameters and Gazebo
+            r, p, y = symbols('r p y')
+
+            ROT_X = Matrix([[1,      0,       0],
+                            [0, cos(r), -sin(r)],
+                            [0, sin(r),  cos(r)]])  #ROLL
+
+            ROT_Y = Matrix([[cos(p),  0, sin(p)],
+                            [     0,  1,      0],
+                            [-sin(p), 0, cos(p)]]) #PITCH
+
+            ROT_Z = Matrix([[cos(y), -sin(y), 0],
+                            [sin(y),  cos(y), 0],
+                            [0,            0, 1]]) #YAW
+
+            ROT_EE = ROT_Z * ROT_Y * ROT_X
+
+            ROT_Error = ROT_Z.subs(y, radians(180)) * ROT_Y.subs(p, radians(-90))
+
+            ROT_EE = ROT_EE * ROT_Error
+            ROT_EE = ROT_EE.subs({'r': roll, 'p': pitch, 'y': yaw})
 
             EE = Matrix([[px],
                          [py],
@@ -108,10 +113,6 @@ def handle_calculate_IK(req):
 
             WC = EE - (0.303) * ROT_EE[:,2]
 
-            ### Your IK code here
-	    # Compensate for rotation discrepancy between DH parameters and Gazebo
-	    #
-	    #
             #Calculate joint angles using Geometric IK method
             theta1 = atan2(WC[1], WC[0])
    
@@ -135,6 +136,9 @@ def handle_calculate_IK(req):
             theta4 = atan2(R3_6[2,2], -R3_6[0,2])
             theta5 = atan2(sqrt(R3_6[0,2] * R3_6[0,2] + R3_6[2,2]*R3_6[2,2]), R3_6[1,2])
             theta6 = atan2(-R3_6[1,1], R3_6[1,0])
+
+            # Populate response for the IK request
+            # In the next line replace theta1,theta2...,theta6 by your joint angle variables
 	    joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
 	    joint_trajectory_list.append(joint_trajectory_point)
 
